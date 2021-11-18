028ff64b6b4f4638f2c6980befc52f2af888ce03
sudha020 <89408777+sudha020@users.noreply.github.com>
2021-11-18 10:55:50 -0600

    added code

--- routes/resource.js
+++ routes/resource.js
@@ -33,3 +33,7 @@ module.exports = router;
 router.get('/create', umbrella_controlers.umbrella_create_Page); 
 /* GET create update page */ 
 router.get('/update', umbrella_controlers.umbrella_update_Page); 
+/* GET create update page */ 
+
+/* GET create umbrella page */ 
+router.get('/delete', umbrella_controlers.umbrella_delete_Page); 
